280ccb16da4f88cedea337f0df7f6d73557fab2d
mitalletap <54956315+mitalletap@users.noreply.github.com>
2020-05-08 19:45:59 -0500

    port update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 8080;
+const PORT = 80;
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
