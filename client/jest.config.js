b2a1c8978970cd492483fd7d349a05383352344d
mitalletap <54956315+mitalletap@users.noreply.github.com>
2020-05-10 14:30:09 -0500

    Jenkins Test

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     "collectCoverage" : true,
-    collectCoverageFrom: [
+    'collectCoverageFrom': [
       "src/**/*.{js,jsx}",
       "!src/index.js",
       "!src/serviceWorker.js"
