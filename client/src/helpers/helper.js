5a6456865615eb178abaa5cd0ef43606b51de261
mitalletap <54956315+mitalletap@users.noreply.github.com>
2020-05-15 01:11:28 -0500

    proxy changes

--- client/src/helpers/helper.js
+++ client/src/helpers/helper.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 const HelperFunction = {
 
     getEnvironmentStatus: function() {
-        const eVar = process.env.NODE_ENV === 'production' ? '3.20.212.154:8080' : 'localhost:8080';
+        const eVar = process.env.NODE_ENV === 'production' ? 'mitalletap.io:8080' : 'localhost:8080';
         return eVar;
     } 
 
